6d7b3822fd298fefb53db96b7eea46b87687cc45
Elvis Landi <elandi13@yahoo.com>
2020-09-26 15:51:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,4 +9,6 @@ index_0 = win_combo[0]
 index_1 = win_combo[1]
 index_2 = win_combo[2]
 
-postion_1 = board[index_0]
+position_1 = board[index_0]
+position_2 = board[index_1]
+position_3 = board[index_2]
